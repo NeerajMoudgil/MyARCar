155ced903bdd19a5f667247f763db76d99f38fae
Neeraj Moudgil <neerajcse7@gmail.com>
2018-08-20 14:38:17 +0530

    manifest changes and intialise ARfragment

--- app/src/main/java/com/example/neerajm/myarcar/MainActivity.java
+++ app/src/main/java/com/example/neerajm/myarcar/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.neerajm.myarcar;
 
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    Session mSession;
+    private ArFragment arFragment;
+    private ArSceneView arSceneView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arFragment= (ArFragment)  getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
+
+        // hiding the plane discovery
+        arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getPlaneDiscoveryController().setInstructionView(null);
+
+        arSceneView= arFragment.getArSceneView();
+
     }
+
+
+
 }
